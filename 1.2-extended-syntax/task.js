0f505bcd432f8c85d0f4dd561bab76b54c17a020
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-01 20:56:23 +0300

    Первый коммит

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,15 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+  "use strict";
+  // код для задачи №1 писать здесь 2x^2+4x-3=0
+  // return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  // код для задачи №2 писать здесь
+  // return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+  // код для задачи №3 писать здесь
+  // return result;
 }
