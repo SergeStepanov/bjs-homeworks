d3e1e28443147c26c1361a9c4a1d46b9658c47ab
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-04 01:09:08 +0300

    Решена 1 задача

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,15 +1,44 @@
 function getResult(a, b, c) {
-  "use strict";
+  'use strict';
+
   // код для задачи №1 писать здесь 2x^2+4x-3=0
-  // return x;
+  let D = b ** 2 - 4 * a * c;
+  let x = [];
+  let d = Math.sqrt(D); // Math.sqrt(D) расчет корня
+
+  if (D < 0) {
+    x = [];
+  } else if (D === 0) {
+    x.push((-b - d) / (2 * a));
+  } else if (D > 0) {
+    x.push((-b + d) / (2 * a), (-b - d) / (2 * a));
+  }
+
+  return x;
 }
 
 function getAverageMark(marks) {
   // код для задачи №2 писать здесь
-  // return averageMark;
+
+  let averageMark = marks / marks.length;
+
+  if (marks.length === 0) {
+    averageMark = 0;
+  } else if (marks.length <= 5) {
+  }
+  // console.log(
+  //   `Количество оценок больше 5. Счтитаеся ср. оценка только первых 5-ти оценок.`
+  // );
+
+  return averageMark;
 }
 
 function askDrink(name, dateOfBirthday) {
   // код для задачи №3 писать здесь
+
+  let newDate = {};
+
+  function getFullYear() {}
+
   // return result;
 }
