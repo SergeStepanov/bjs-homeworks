811dab1ef7f20e5b3ba8ae2385cc439f1e776e80
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-08 00:31:36 +0300

    Правки к ДЗ

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,17 +1,18 @@
+'use strict';
+
 function getResult(a, b, c) {
-  'use strict';
-
   // код для задачи №1 писать здесь 2x^2+4x-3=0
-  let D = b ** 2 - 4 * a * c;
+  let discriminant = b ** 2 - 4 * a * c;
   let x = [];
-  let d = Math.sqrt(D); // Math.sqrt(D) расчет корня
+  let squareRootDiscriminant = Math.sqrt(discriminant); // Math.sqrt(number) расчет корня
 
-  if (D < 0) {
-    x = [];
-  } else if (D === 0) {
-    x.push((-b - d) / (2 * a));
-  } else if (D > 0) {
-    x.push((-b + d) / (2 * a), (-b - d) / (2 * a));
+  if (discriminant === 0) {
+    x.push((-b - squareRootDiscriminant) / (2 * a));
+  } else if (discriminant > 0) {
+    x.push(
+      (-b + squareRootDiscriminant) / (2 * a),
+      (-b - squareRootDiscriminant) / (2 * a)
+    );
   }
 
   return x;
@@ -19,51 +20,34 @@ function getResult(a, b, c) {
 
 function getAverageMark(marks) {
   // код для задачи №2 писать здесь
-
-  let len = marks.length;
-  let averageMark = marks / len;
-
-  if (len === 0) {
-    averageMark = 0;
-  }
-
+  let averageMark;
   let sum = 0;
 
-  for (let i = 0; i < len && len <= 5; i++) {
-    sum += marks[i];
-    averageMark = sum / len;
+  if (marks.length === 0) {
+    return 0;
   }
 
-  if (len > 5) {
+  if (marks.length > 5) {
+    marks.splice(5);
     console.log(
       `Количество оценок больше 5. Счтитаеся ср. оценка только первых 5-ти оценок.`
     );
   }
 
-  for (let i = 0; i < 5 && len > 5; i++) {
-    marks.splice(5);
+  for (let i = 0; i < marks.length; i++) {
     sum += marks[i];
-    averageMark = sum / 5;
+    averageMark = sum / marks.length;
   }
-
   return averageMark;
 }
 
 function askDrink(name, dateOfBirthday) {
   // код для задачи №3 писать здесь
-
-  let today = new Date();
-  let year = today.getFullYear();
-  let yearBirthday = dateOfBirthday.getFullYear();
-  let result = year - yearBirthday;
-
-  if (result >= 18) {
-    alert(`Не желаете ли олд-фэшн, ${name}?`);
+  let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+  if (age < 18) {
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
   } else {
-    alert(
-      `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`
-    );
+    return `Не желаете ли олд-фэшн, ${name}?`;
   }
-
-  return result;
+  // return result;
 }
