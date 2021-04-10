f762320a23cda1506d196aaafe7b15b37d1fa515
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-11 02:23:41 +0300

    Решение ДЗ

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,49 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  /* процентная ставка, сумма первоначального взноса, сумма кредита, срок (дата окончания кредита)
+  Платеж=S*(P+P/(((1+P)^n)-1))
+  */
+  // код для задачи №1 писать здесь
+  Number.parseFloat(percent);
+
+  if (percent === NaN) {
+    return `Параметр 'percent' содержит неправильное значение ${percent}`;
+  }
+
+  Number.parseFloat(contribution);
+
+  if (contribution === NaN) {
+    return `Параметр 'contribution' содержит неправильное значение ${contribution}`;
+  }
+
+  Number.parseFloat(amount);
+
+  if (amount === NaN) {
+    return `Параметр 'amount' содержит неправильное значение ${amount}`;
+  }
+
+  let loanBody = amount - contribution; // тело кредита
+  let numberOfMonths = date.getMonth() - new Date().getMonth(); // кол-во месяцев
+  let percentMonth = percent / 12; // 1/12 процентной ставки P
+  let payment =
+    loanBody *
+    (percentMonth + percentMonth / ((1 + percentMonth) ** numberOfMonths - 1)); // платеж
+  let totalAmount = payment * numberOfMonths;
+
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  // код для задачи №2 писать здесь
+
+  if (name === '' || name === undefined) {
+    console.log(`Привет, мир! Меня зовут Аноним.`);
+
+    return `Привет, мир! Меня зовут Аноним.`;
+  } else {
+    console.log(`Привет, мир! Меня зовут ${name}.`);
+
+    return `Привет, мир! Меня зовут ${name}.`;
+  }
 }
