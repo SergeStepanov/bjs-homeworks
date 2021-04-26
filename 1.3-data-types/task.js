7a11f9884d09b68b5fbd2d7294334091394fb450
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-26 20:46:11 +0300

    НЕТ решения ДЗ

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -5,27 +5,34 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   Платеж=S*(P+P/(((1+P)^n)-1))
   */
   // код для задачи №1 писать здесь
-  Number.parseFloat(percent);
 
-  if (percent === NaN) {
+  if (typeof percent === undefined || percent <= 0) {
     return `Параметр 'percent' содержит неправильное значение ${percent}`;
   }
 
-  Number.parseFloat(contribution);
-
-  if (contribution === NaN) {
+  if (typeof contribution === undefined || contribution <= 0) {
     return `Параметр 'contribution' содержит неправильное значение ${contribution}`;
   }
 
-  Number.parseFloat(amount);
-
-  if (amount === NaN) {
+  if (typeof amount === undefined || amount <= 0) {
     return `Параметр 'amount' содержит неправильное значение ${amount}`;
   }
 
-  let loanBody = amount - contribution; // тело кредита
-  let numberOfMonths = date.getMonth() - new Date().getMonth(); // кол-во месяцев
-  let percentMonth = percent / 12; // 1/12 процентной ставки P
+  let numPercent = Number(percent);
+  let numContribution = Number(contribution);
+  let numAmount = Number(amount);
+  let nowDate = new Date();
+
+  // if (date.getTime() <= nowDate.getTime()) {
+  //   return `Дата введена не корректно`;
+  // }
+
+  let loanBody = numAmount - numContribution; // тело кредита
+  let numberOfMonths =
+    date.getMonth() -
+    nowDate.getMonth() +
+    12 * (date.getFullYear() - nowDate.getFullYear()); // кол-во месяцев
+  let percentMonth = numPercent / 100 / 12; // 1/12 процентной ставки P
   let payment =
     loanBody *
     (percentMonth + percentMonth / ((1 + percentMonth) ** numberOfMonths - 1)); // платеж
@@ -36,14 +43,13 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 function getGreeting(name) {
   // код для задачи №2 писать здесь
+  let greeting = `Привет, мир! Меня зовут ${name}.`;
 
-  if (name === '' || name === undefined) {
-    console.log(`Привет, мир! Меня зовут Аноним.`);
-
-    return `Привет, мир! Меня зовут Аноним.`;
-  } else {
-    console.log(`Привет, мир! Меня зовут ${name}.`);
-
-    return `Привет, мир! Меня зовут ${name}.`;
+  if (name === undefined || name === '') {
+    name = 'Аноним';
   }
+
+  console.log(greeting);
+
+  return greeting;
 }
