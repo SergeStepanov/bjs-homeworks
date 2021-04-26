1b722669eb313ae0a430e10b8462a768b6dcdc13
Sergey Stepanov <ser1653285@yandex.ru>
2021-04-26 20:41:44 +0300

    Решение ДЗ 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
+'use strict';
+
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+  let originStr = this.toLowerCase().split('').join('').replace(/\s/g, '');
+  let reversStr = this.toLowerCase()
+    .split('')
+    .reverse()
+    .join('')
+    .replace(/\s/g, '');
+
+  if (originStr === reversStr) {
+    return true;
+  }
+  return false;
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  // код для задачи №2 писать здесь
+
+  let average;
+  let sum = 0;
+
+  if (marks.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+    average = sum / marks.length;
+  }
+
+  let roundedAverage = Math.round(average);
+
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  // код для задачи №3 писать здесь
+  // return verdict
 }
